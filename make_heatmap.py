503d629cbeef2adeaed05c31d9ae17d2257ff9f5
snkeegan <sarah@fenyolab.org>
2022-04-27 07:40:00 -0400

    Refining stats and heatmap

--- make_heatmap.py
+++ make_heatmap.py
@@ -1,21 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
-import re
 import seaborn as sns
 import numpy as np
 
-root_dir = "/Users/snk218/Dropbox/mac_files/fenyolab/data_and_results/Rona_FRAP/final_results/Organized BER recruitment data only"
+root_dir = "/Users/sarahkeegan/Dropbox/mac_files/fenyolab/data_and_results/Rona_FRAP/final_results/Organized BER recruitment data only"
 
-data_for_hm=pd.read_csv(f"{root_dir}/all_for_heatmap.csv")
-data_for_hm.set_index('Time (s)',drop=True,inplace=True)
+data_for_hm=pd.read_csv(f"{root_dir}/all_for_heatmap_NORM.csv")
+data_for_hm['Time (m)']=np.round(data_for_hm['Time (s)']/60,1)
+data_for_hm.drop(columns=['Time (s)'],inplace=True)
+data_for_hm.set_index('Time (m)',drop=True,inplace=True)
 
+df = data_for_hm.rolling(6).mean()
+df_ = df.iloc[::6, :]
+df_ = df_.iloc[1:,:]
 
-for protein in data_for_hm.columns:
-    print(protein)
-    print(np.percentile(data_for_hm[protein], [25,50,75]))
-
-fig,ax=plt.subplots()
-sns.clustermap(data_for_hm.transpose(), center=0, col_cluster=False, vmin=-50, vmax=50)
-fig.tight_layout()
-fig.savefig(f"{root_dir}/all_heatmap.png")
+sns.clustermap(df_.transpose(), center=0, col_cluster=False, method='ward', vmin=-0.3, vmax=0.3, cmap=sns.diverging_palette(240, 10, n=36))
+plt.tight_layout()
+plt.savefig(f"{root_dir}/all_heatmap.png")
--- stats_for_proteins.py
+++ stats_for_proteins.py
@@ -6,30 +6,29 @@ import seaborn as sns
 import numpy as np
 from scipy.stats import mannwhitneyu
 
-root_dir = "/Users/snk218/Dropbox/mac_files/fenyolab/data_and_results/Rona_FRAP/final_results/Organized BER recruitment data only"
+root_dir = "/Users/sarahkeegan/Dropbox/mac_files/fenyolab/data_and_results/Rona_FRAP/final_results/Organized BER recruitment data only"
 n_timepoints=242
 
 dir_list = next(os.walk(root_dir))[1]
 num_proteins = len(dir_list)
-make_plot=False
-start_t = 50
-end_t = 400
+make_plot=True
+
 pvalue_matrix=[]
 fig, axs = plt.subplots(nrows=7, ncols=5, figsize=(20,15),sharex=True) # room for 35 proteins (there are 31)
+fig2, axs2 = plt.subplots(nrows=7, ncols=5, figsize=(20,15),sharex=True) # room for 35 proteins (there are 31)
 row_i=0
 col_i=0
-data_for_hm = pd.DataFrame()
-for i,cur_dir in enumerate(dir_list):
-    #if(not os.path.exists(f"{root_dir}/{cur_dir}/SplitData.xlsx")):
-    #    print(f"Error: did not locate SplitData.xlsx in {cur_dir}. Skipping...")
-    #    continue
 
+data_for_hm_norm = pd.DataFrame()
+
+for i,cur_dir in enumerate(dir_list):
     df_all=pd.read_excel(f"{root_dir}/{cur_dir}/SplitData.xlsx", sheet_name="SIALLDIFF", nrows=n_timepoints+1)
     df_ctrl=pd.read_excel(f"{root_dir}/{cur_dir}/SplitData.xlsx", sheet_name="SICTRLDIFF", nrows=n_timepoints+1)
 
     protein_name = re.split("_",cur_dir,)[1]
     print(protein_name)
 
+    # PROCESSING INPUT
     # removed "Unnamed", "mean"
     df_all.drop(columns=df_all.filter(regex=r"^Unnamed").columns,inplace=True)
     df_ctrl.drop(columns=df_ctrl.filter(regex=r"^Unnamed").columns,inplace=True)
@@ -51,6 +50,7 @@ for i,cur_dir in enumerate(dir_list):
     else:
         df_ctrl.drop(null_idx,axis=0,inplace=True)
 
+    # MELT THE DATA FRAMES
     df_all=df_all.melt(id_vars=['Time (s)'], value_vars=df_all.columns[df_all.columns != "Time (s)"], var_name='cell', value_name='intensity-diff')
     df_all['cell']=df_all['cell'].astype('str') + '_0'
     df_all['condition']='siALL'
@@ -61,61 +61,101 @@ for i,cur_dir in enumerate(dir_list):
 
     df_full=pd.concat([df_all,df_ctrl], ignore_index=True)
 
-    # Plot mean with error shading of intensity-difference over time, for each protein
-    if(make_plot):
-        sns.lineplot(data=df_full, x='Time (s)', y='intensity-diff', hue='condition', ax=axs[row_i][col_i])
-        axs[row_i][col_i].set_title(protein_name)
+    # STATISTICS OVER TIME WINDOWS FROM START TO FINISH
+    # Take the mean intensity of siALL/siCtrl for time windows of 50s
+    # Use Mann Whitney to test for significance and get a p-value for each time window
+    gap = 50
+    times_sig=[]
+    times_sig_=[]
+    for start_t in range(25, 625 + gap, gap):
+        end_t = start_t + gap
 
-        axs[row_i][col_i].axvline(50,ls='--',color='black')
-        axs[row_i][col_i].axvline(400,ls='--',color='black')
-
-        if (not (row_i == 0 and col_i == 0)):
-            axs[row_i][col_i].get_legend().remove()
-
-    # Heatmap
-    # mean all cells for each time point
-    # normalize between 0 and 1 (1 is the max Intensity of all values, both siAll and siCtrl)
-    df_all_for_hm=df_all.groupby('Time (s)').mean()['intensity-diff']
-    df_ctrl_for_hm=df_ctrl.groupby('Time (s)').mean()['intensity-diff']
-
-    max_val=np.max([df_ctrl_for_hm.max(),df_all_for_hm.max()])
-    df_all_for_hm=df_all_for_hm/max_val
-    df_ctrl_for_hm=df_ctrl_for_hm/max_val
-
-    data_for_hm[protein_name]=df_all_for_hm-df_ctrl_for_hm
-
-    # Subtract the intensity-difference at time 0 from each intensity
-    # Take the mean intensity of siALL from time=50s to time=400s
-    # Take the mean intensity of siCtrl from time=50s to time=400s
-    # Use ? to test for significance and get a p-value
-    gap=50
-    for start_t in range(25,625+gap,gap):
-        end_t=start_t+gap
-
-        df_all_filt=df_all[(df_all['Time (s)']>=start_t) & (df_all['Time (s)']<=end_t)]
-        df_all_data=df_all_filt.groupby('cell').mean()['intensity-diff']
+        df_all_filt = df_all[(df_all['Time (s)'] >= start_t) & (df_all['Time (s)'] <= end_t)]
+        df_all_data = df_all_filt.groupby('cell').mean()['intensity-diff']
 
         df_ctrl_filt = df_ctrl[(df_ctrl['Time (s)'] >= start_t) & (df_ctrl['Time (s)'] <= end_t)]
         df_ctrl_data = df_ctrl_filt.groupby('cell').mean()['intensity-diff']
 
-        orig_len=len(df_all_data)
-        df_all_data=df_all_data[df_all_data>0]
-        print(f"siAll: Negatives removed: {orig_len-len(df_all_data)} ({len(df_all_data)})")
+        orig_len = len(df_all_data)
+        df_all_data = df_all_data[df_all_data > 0]
+        print(f"siAll: Negatives removed: {orig_len - len(df_all_data)} ({len(df_all_data)})")
 
-        orig_len=len(df_ctrl_data)
+        orig_len = len(df_ctrl_data)
         df_ctrl_data = df_ctrl_data[df_ctrl_data > 0]
-        print(f"siCtrl: Negatives removed: {orig_len-len(df_ctrl_data)} ({len(df_ctrl_data)})")
+        print(f"siCtrl: Negatives removed: {orig_len - len(df_ctrl_data)} ({len(df_ctrl_data)})")
 
         # Use Mann Whitney?
         U1, p = mannwhitneyu(df_all_data, df_ctrl_data)
-        #print(p)
+        # print(p)
+        if(p < 0.05):
+            sig=1
+            times_sig_.append(start_t)
+        else:
+            sig=0
+            if(len(times_sig_)>0):
+                times_sig.append(times_sig_)
+                times_sig_=[]
 
         pvalue_matrix.append([
             protein_name,
             start_t,
             end_t,
             p,
+            sig
         ])
+    if(times_sig_!=[]):
+        times_sig.append(times_sig_)
+
+    p_times=[]
+    if(len(times_sig) > 0):
+        times_sig_len=[len(x) for x in times_sig]
+        max_index = times_sig_len.index(max(times_sig_len))
+        times_sig=times_sig[max_index]
+
+        p_times.append(times_sig[0])
+        p_times.append(times_sig[-1]+gap)
+
+
+    # PLOT
+    # Plot mean with error shading of intensity-difference over time, for each protein
+    if(make_plot):     # (have a flag here b/c plot takes a long time to make)
+        sns.lineplot(data=df_full, x='Time (s)', y='intensity-diff', hue='condition', ax=axs[row_i][col_i])
+        axs[row_i][col_i].set_title(protein_name)
+
+        if(p_times != []):
+
+            axs[row_i][col_i].axvline(p_times[0],ls='--',color='black')
+            axs[row_i][col_i].axvline(p_times[1],ls='--',color='black')
+
+        if (not (row_i == 0 and col_i == 0)):
+            axs[row_i][col_i].get_legend().remove()
+
+    # MEANS AND NORMALIZATION 0-1
+    # ANOTHER PLOT - just showing norm'd means
+    # SAVE DATA FOR HEATMAP
+    # mean all cells for each time point
+    # value at each time point = mean(siAll)-mean(siCtrl)
+    df_mean = pd.DataFrame()
+    df_mean['Time (s)'] = df_all['Time (s)'].unique()
+    df_mean.index=df_mean['Time (s)']
+    df_mean['siAll'] = df_all.groupby('Time (s)').mean()['intensity-diff']
+    df_mean['siCtrl'] = df_ctrl.groupby('Time (s)').mean()['intensity-diff']
+
+    # divide by max value over siAll and siCtrl so that data will be between 0 and 1
+    max_val=np.max([df_mean['siAll'].max(),df_mean['siCtrl'].max()])
+    min_val = np.max([df_mean['siAll'].min(), df_mean['siCtrl'].min()])
+
+    df_mean['siAll-norm'] = (df_mean['siAll']-min_val) / (max_val-min_val)
+    df_mean['siCtrl-norm'] = (df_mean['siCtrl']-min_val) / (max_val-min_val)
+
+    # Plotting the normalized data:
+    sns.lineplot(data=df_mean, x='Time (s)', y='siAll-norm', ax=axs2[row_i][col_i])
+    sns.lineplot(data=df_mean, x='Time (s)', y='siCtrl-norm', ax=axs2[row_i][col_i])
+    axs2[row_i][col_i].set_title(protein_name)
+    if (row_i == 0 and col_i == 0):
+        axs2[row_i][col_i].legend()
+
+    data_for_hm_norm[protein_name]=df_mean['siAll-norm'] - df_mean['siCtrl-norm']
 
     if ((row_i + 1) % 7 == 0):
         row_i = 0
@@ -123,10 +163,14 @@ for i,cur_dir in enumerate(dir_list):
     else:
         row_i += 1
 
-pvalue_df = pd.DataFrame(pvalue_matrix, columns=['protein','start_t','end_t','p-value'])
+# SAVE STATS, HEATMAP DATA, PLOTS
+pvalue_df = pd.DataFrame(pvalue_matrix, columns=['protein','start_t','end_t','p-value','sig'])
 pvalue_df.to_csv(f"{root_dir}/all_pvalues.csv")
 
-data_for_hm.to_csv(f"{root_dir}/all_for_heatmap2.csv")
+data_for_hm_norm.to_csv(f"{root_dir}/all_for_heatmap_NORM.csv")
+
+fig2.tight_layout()
+fig2.savefig(f"{root_dir}/all_norm.png")
 
 if(make_plot):
     fig.tight_layout()
